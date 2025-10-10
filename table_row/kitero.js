def520b5158709a605c16b19f68972eb9bdc7e9c
Vas Zoltan Dominik <vas.zoltan@BOLYAI.LOCAL>
2025-10-10 08:55:28 +0200

    órai feladat

---
+++ table_row/kitero.js
@@ -0,0 +1,17 @@
+const arr = {}
+let szam = 4;
+if(szam>5)
+{
+    arr.name = "rah";
+}
+else
+{
+    arr["name"] = "segítség"
+}
+console.log(arr)
+console.log(arr["name"])
+
+if(arr.age == undefined) //if(arr.age) ha arr.age nem egyenlő nulla akkor elég
+{
+
+}
